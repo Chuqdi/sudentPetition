c3d59a1c9627ef104d9a3a938a2e44ac6e83cf3f
gbengarock <temidayoolugbenga@yahoo.com>
2022-12-05 20:14:49 +0100

    add new pages

--- student_management_app/views.py
+++ student_management_app/views.py
@@ -17,27 +17,55 @@ def home(request):
     return render(request, 'index.html')
 
 
+def about(request):
+    return render(request, 'about.html')
+
+
+def classes(request):
+    return render(request, 'class.html')
+
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def blog(request):
+    return render(request, 'blog.html')
+
+
+def single(request):
+    return render(request, 'single.html')
+
+
+def team(request):
+    return render(request, 'team.html')
+
+
 def loginPage(request):
     return render(request, 'login.html')
-
 
 
 def doLogin(request):
     if request.method != "POST":
         return HttpResponse("<h2>Method Not Allowed</h2>")
     else:
-        user = EmailBackEnd.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
+        user = EmailBackEnd.authenticate(request, username=request.POST.get(
+            'email'), password=request.POST.get('password'))
         if user != None:
             login(request, user)
             user_type = user.user_type
-            #return HttpResponse("Email: "+request.POST.get('email')+ " Password: "+request.POST.get('password'))
+            # return HttpResponse("Email: "+request.POST.get('email')+ " Password: "+request.POST.get('password'))
             if user_type == '1':
                 return redirect('admin_home')
-                
+
             elif user_type == '2':
                 # return HttpResponse("Staff Login")
                 return redirect('staff_home')
-                
+
             elif user_type == '3':
                 # return HttpResponse("Student Login")
                 return redirect('student_home')
@@ -46,9 +74,8 @@ def doLogin(request):
                 return redirect('login')
         else:
             messages.error(request, "Invalid Login Credentials!")
-            #return HttpResponseRedirect("/")
+            # return HttpResponseRedirect("/")
             return redirect('login')
-
 
 
 def get_user_details(request):
@@ -58,17 +85,41 @@ def get_user_details(request):
         return HttpResponse("Please Login First")
 
 
-
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect('/')
 
 
-
-
-
 def home(request):
     return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def classes(request):
+    return render(request, 'class.html')
+
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def blog(request):
+    return render(request, 'blog.html')
+
+
+def single(request):
+    return render(request, 'single.html')
+
+
+def team(request):
+    return render(request, 'team.html')
 
 
 def loginPage(request):
